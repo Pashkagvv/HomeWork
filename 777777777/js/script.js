606519631d48a4157376230a6c544b802ae1cb67
Pashkagvv <pashkagvv@yahoo.com>
2019-04-24 09:03:45 +0300

    add 7777 project

---
+++ 777777777/js/script.js
@@ -0,0 +1,42 @@
+let tab, contentWrapper;
+
+
+
+window.onload = function () {
+    tab = document.getElementsByClassName('tab');
+    content = document.getElementsByClassName('contentWrapper');
+    notVisiblContent(1);
+}
+ 
+
+function notVisiblContent(x) {
+    for (var i = x; i < content.length; i++) {
+       content[i].classList.remove('seen');
+       content[i].classList.add('notSeen');
+       tab[i].classList.remove('active');
+    }
+    
+}
+
+function VisiblContent(i) {
+    if (content[i].classList.contains('notSeen')) {
+        notVisiblContent(0);
+        tab[i].classList.add('active');
+        content[i].classList.remove('notSeen');
+        content[i].classList.add('seen');
+
+    }
+}
+
+document.getElementById('tabs').onclick = function(event){
+     var target = event.target;
+     if (target.className == 'tabs') {
+         for (var i = 1; i < tab.length; i++) {
+            if(target == tab[i]){
+                VisiblContent(i);
+                break;
+            }
+             
+         }
+     }
+ }
